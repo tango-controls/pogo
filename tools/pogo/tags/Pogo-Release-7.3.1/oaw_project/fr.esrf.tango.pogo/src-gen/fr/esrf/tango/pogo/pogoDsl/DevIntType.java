1215a1c11f7e123f0914ea16c8aacd426b969bc1
pascal_verdier <pascal_verdier@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-08-26 12:45:06 +0000

    Pb with dev_state() method in case of ALARM state fixed. Pb in case of main.cpp missing (convertion from pogo-6) fixed. Buttons for java projects added.

---
+++ tools/pogo/tags/Pogo-Release-7.3.1/oaw_project/fr.esrf.tango.pogo/src-gen/fr/esrf/tango/pogo/pogoDsl/DevIntType.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ */
+package fr.esrf.tango.pogo.pogoDsl;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Dev Int Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see fr.esrf.tango.pogo.pogoDsl.PogoDslPackage#getDevIntType()
+ * @model
+ * @generated
+ */
+public interface DevIntType extends Type
+{
+} // DevIntType
