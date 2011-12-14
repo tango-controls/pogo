57db28ccfce211c9ee3135e68affb97a0f3e3f20
kthoms <kthoms@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2011-12-14 08:35:10 +0000

    Adding protected region support

--- tools/pogo/branches/workshop_dec2011/oaw_project/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/PogoGeneratorModule.java
+++ tools/pogo/branches/workshop_dec2011/oaw_project/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/PogoGeneratorModule.java
@@ -1,5 +1,8 @@
 package fr.esrf.tango.pogo.generator;
 
+import java.util.List;
+
+import net.danieldietrich.protectedregions.core.IRegionParser;
 import net.danieldietrich.protectedregions.core.RegionParserBuilder;
 import net.danieldietrich.protectedregions.core.RegionParserFactory;
 import net.danieldietrich.protectedregions.support.ProtectedRegionSupport;
@@ -9,6 +12,7 @@ import org.eclipse.xtext.generator.IGenerator;
 import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
 import org.eclipse.xtext.service.AbstractGenericModule;
 
+import com.google.common.collect.Lists;
 import com.google.inject.Provides;
 
 import fr.esrf.tango.pogo.generator.pr.PogoPR;
@@ -35,10 +39,30 @@ public class PogoGeneratorModule extends AbstractGenericModule {
 	public JavaIoFileSystemAccess createJavaIoFileSystemAccess(ProtectedRegionSupport support) {
 	  support.addParser(RegionParserFactory.createJavaParser(new PogoPR(),false), ".java");
 	  support.addParser(RegionParserFactory.createXmlParser(), ".xml", ".xsd");
-	  support.addParser(new RegionParserBuilder().name("cpp").addComment("/*", "*/").addComment("//").ignoreCData('"', '\\')
-				.ignoreCData('\'', '\\').setInverse(false).useOracle(new PogoPR()).build());
+	  for (IRegionParser parser: createCppParsers()) {
+		  support.addParser(parser);
+	  }
+	  support.addParser(createPythonParser());
 	  BidiJavaIoFileSystemAccess fsa = new BidiJavaIoFileSystemAccess(support);
 	  // fsa.setFilter(...); // (optional)
 	  return fsa;
+	}
+	
+	private IRegionParser createPythonParser () {
+		IRegionParser parser = new RegionParserBuilder().name("py").addComment("#").ignoreCData('"', '\\')
+				.ignoreCData('\'', '\\').setInverse(false).useOracle(new PogoPR()).build();
+		return parser;
+	}
+	
+	private List<IRegionParser> createCppParsers () {
+		String[] extensions = new String[] {"cpp","hpp","h", "cxx","hxx"};
+		
+		List<IRegionParser> parsers = Lists.newArrayList();
+		for (String extension: extensions) {
+			IRegionParser parser = new RegionParserBuilder().name(extension).addComment("/*", "*/").addComment("//").ignoreCData('"', '\\')
+					.ignoreCData('\'', '\\').setInverse(false).useOracle(new PogoPR()).build();
+			parsers.add(parser);
+		}
+		return parsers;
 	}
 }
