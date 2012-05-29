c9c4d3b6c28b520b92fdc20e3cda497e6af07914
pascal_verdier <pascal_verdier@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2012-05-29 08:55:45 +0000

    Improve vc10 project. Modify file filter for java classes.

---
+++ tools/pogo/tags/Pogo-Release-8.0.3/oaw_project/fr.esrf.tango.pogo/src-gen/fr/esrf/tango/pogo/pogoDsl/impl/ConstStringTypeImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ */
+package fr.esrf.tango.pogo.pogoDsl.impl;
+
+import fr.esrf.tango.pogo.pogoDsl.ConstStringType;
+import fr.esrf.tango.pogo.pogoDsl.PogoDslPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Const String Type</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class ConstStringTypeImpl extends TypeImpl implements ConstStringType
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected ConstStringTypeImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return PogoDslPackage.Literals.CONST_STRING_TYPE;
+  }
+
+} //ConstStringTypeImpl
