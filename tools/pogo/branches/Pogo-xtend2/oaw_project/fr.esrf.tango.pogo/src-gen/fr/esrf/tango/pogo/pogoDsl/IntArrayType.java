3a9adce39808166cfc3eec1bbd74b03be9bee129
pascal_verdier <pascal_verdier@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2012-06-04 11:39:33 +0000

    Xpand to Xtend2 conversion.

---
+++ tools/pogo/branches/Pogo-xtend2/oaw_project/fr.esrf.tango.pogo/src-gen/fr/esrf/tango/pogo/pogoDsl/IntArrayType.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ */
+package fr.esrf.tango.pogo.pogoDsl;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Int Array Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see fr.esrf.tango.pogo.pogoDsl.PogoDslPackage#getIntArrayType()
+ * @model
+ * @generated
+ */
+public interface IntArrayType extends Type
+{
+} // IntArrayType
---
+++ tools/pogo/branches/Pogo-xtend2/oaw_project/fr.esrf.tango.pogo/src-gen/fr/esrf/tango/pogo/pogoDsl/impl/DoubleVectorTypeImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ */
+package fr.esrf.tango.pogo.pogoDsl.impl;
+
+import fr.esrf.tango.pogo.pogoDsl.DoubleVectorType;
+import fr.esrf.tango.pogo.pogoDsl.PogoDslPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Double Vector Type</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class DoubleVectorTypeImpl extends VectorTypeImpl implements DoubleVectorType
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected DoubleVectorTypeImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return PogoDslPackage.Literals.DOUBLE_VECTOR_TYPE;
+  }
+
+} //DoubleVectorTypeImpl
