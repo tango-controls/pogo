6bed9382742d06a32904694105dcef02708a4f7c
Pascal Verdier <verdier@esrf.fr>
2012-06-05 07:00:19 +0000

    Launch cpp added.

--- oaw_project/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/PogoGeneratorMain.java
+++ oaw_project/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/PogoGeneratorMain.java
@@ -1,6 +1,7 @@
 package fr.esrf.tango.pogo.generator;
 
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -13,11 +14,16 @@ import org.apache.commons.cli.ParseException;
 import org.eclipse.emf.mwe2.launch.runtime.Mwe2Launcher;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 public class PogoGeneratorMain {
 	/** The value of 'module' in the .mwe2 file to execute */
 	private static final String WORKFLOW_MODULE = "fr.esrf.tango.pogo.generator.PogoDslGeneratorMWE";
-
+	private static final String TYPE_CPP = "cpp";
+	private static final String TYPE_JAVA = "java";
+	private static final String TYPE_PYTHON = "python";
+	private static final String OPT_LANGUAGE = "language";
+	
 	public static void main(String[] args) {
 		int retval = new PogoGeneratorMain().run(args);
 		if (retval != 0)
@@ -38,8 +44,15 @@ public class PogoGeneratorMain {
 						"Generator target directory (default: ./src-gen)")
 				.hasArg().create("targetdir");
 
+		Option optFileTypes = OptionBuilder
+				.withArgName(OPT_LANGUAGE)
+				.withDescription(
+						"Specify target languages (cpp, java, python)")
+				.hasArg().create(OPT_LANGUAGE);
+
 		options.addOption(optSrcDir);
 		options.addOption(optTargetDir);
+		options.addOption(optFileTypes);
 
 		// create the parser
 		final CommandLineParser parser = new GnuParser();
@@ -58,6 +71,15 @@ public class PogoGeneratorMain {
 		launchArgs.add("-pmodelPath=" + line.getOptionValue("srcdir"));
 		launchArgs.add("-ptargetDir="
 				+ line.getOptionValue("targetdir", "./src-gen"));
+		if (line.hasOption(OPT_LANGUAGE)) {
+			Set<String> languages = Sets.newHashSet(line.getOptionValue(OPT_LANGUAGE).split(","));
+			if (languages.contains(TYPE_CPP)) {
+				launchArgs.add("-pgenerateCpp=true");
+			}
+			if (languages.contains(TYPE_JAVA)) {
+				launchArgs.add("-pgenerateJava=true");
+			}
+		}
 		try {
 			Mwe2Launcher.main(launchArgs.toArray(new String[0]));
 			return 0;
