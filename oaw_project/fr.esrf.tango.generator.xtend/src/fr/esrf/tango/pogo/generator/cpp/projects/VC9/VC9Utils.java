d14fa5a88f2eee8baabd9462e11a38cfda4555f1
Pascal Verdier <verdier@esrf.fr>
2012-06-18 12:48:01 +0000

    VC10 project OK

--- oaw_project/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/cpp/projects/VC9/VC9Utils.java
+++ oaw_project/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/cpp/projects/VC9/VC9Utils.java
@@ -1,15 +1,12 @@
 package fr.esrf.tango.pogo.generator.cpp.projects.VC9;
 
-import org.eclipse.emf.common.util.EList;
-import java.util.ArrayList;
 
 import fr.esrf.tango.pogo.pogoDsl.AdditionalFile;
-import fr.esrf.tango.pogo.pogoDsl.Command;
-import fr.esrf.tango.pogo.pogoDsl.Attribute;
-import fr.esrf.tango.pogo.pogoDsl.Inheritance;
 import fr.esrf.tango.pogo.pogoDsl.PogoDeviceClass;
-import fr.esrf.tango.pogo.pogoDsl.State;
 
+/**
+ *	Utilities for VC9 projects
+ */
 
 public class VC9Utils {
 
@@ -46,10 +43,7 @@ public class VC9Utils {
 	//===========================================================
 	//===========================================================
 	public String buildFileLine(String fileName) {
-		StringBuffer	sb = new StringBuffer();
-
-		sb.append("<File RelativePath=\"..\\").append(fileName).append("\"> </File>\n");
-	return sb.toString();
+		return "<File RelativePath=\"..\\" + fileName + "\"> </File>\n";
 	}
 	//===========================================================
 	//===========================================================
