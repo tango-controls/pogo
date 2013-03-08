d4189002beecf2999f1003d48195bedfea98af60
pascal_verdier <pascal_verdier@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-03-08 12:15:51 +0000

    Update file headers

--- tools/pogo/trunk/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/cpp/utils/CppStringUtils.java
+++ tools/pogo/trunk/fr.esrf.tango.generator.xtend/src/fr/esrf/tango/pogo/generator/cpp/utils/CppStringUtils.java
@@ -1,12 +1,45 @@
+//+======================================================================
+//
+// Project:   Tango
+//
+// Description:  source code for Tango code generator.
+//
+// $Author: verdier $
+//
+// Copyright (C) :  2004,2005,2006,2007,2008,2009,2009,2010,2011,2012,2013
+//					European Synchrotron Radiation Facility
+//                  BP 220, Grenoble 38043
+//                  FRANCE
+//
+// This file is part of Tango.
+//
+// Tango is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Tango is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+//
+// $Revision: $
+// $Date:  $
+//
+// $HeadURL: $
+//
+//-======================================================================
+
 package fr.esrf.tango.pogo.generator.cpp.utils;
 
 import org.eclipse.emf.common.util.EList;
 import java.util.ArrayList;
 
-import fr.esrf.tango.pogo.pogoDsl.AdditionalFile;
 import fr.esrf.tango.pogo.pogoDsl.Command;
 import fr.esrf.tango.pogo.pogoDsl.Attribute;
-import fr.esrf.tango.pogo.pogoDsl.Inheritance;
 import fr.esrf.tango.pogo.pogoDsl.PogoDeviceClass;
 import fr.esrf.tango.pogo.pogoDsl.State;
 
