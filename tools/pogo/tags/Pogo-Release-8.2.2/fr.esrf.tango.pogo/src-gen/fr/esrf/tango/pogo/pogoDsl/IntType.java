36bca5699966285210cf45fa86b52a36ddf4a281
pascal_verdier <pascal_verdier@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-10-17 12:00:40 +0000

    Fix a NullPointerException when class come from pogo-6 class without TangoClassID.txt file.

---
+++ tools/pogo/tags/Pogo-Release-8.2.2/fr.esrf.tango.pogo/src-gen/fr/esrf/tango/pogo/pogoDsl/IntType.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ */
+package fr.esrf.tango.pogo.pogoDsl;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Int Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see fr.esrf.tango.pogo.pogoDsl.PogoDslPackage#getIntType()
+ * @model
+ * @generated
+ */
+public interface IntType extends SimpleType, Type
+{
+} // IntType
