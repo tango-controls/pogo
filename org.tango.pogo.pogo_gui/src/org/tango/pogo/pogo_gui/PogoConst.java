67e61077eae6cd1d0b8f998e78bed8836f63e35b
Pascal Verdier <verdier@esrf.fr>
2015-10-02 08:05:19 +0000

    Improve State machine window.

--- org.tango.pogo.pogo_gui/src/org/tango/pogo/pogo_gui/PogoConst.java
+++ org.tango.pogo.pogo_gui/src/org/tango/pogo/pogo_gui/PogoConst.java
@@ -49,7 +49,7 @@ public interface PogoConst {
      * Revision number and date
      */
     public static String revNumber =
-            "9.1.6  -  Thu Sep 17 09:24:38 CEST 2015";
+            "9.1.7  -  Fri Oct 02 09:25:44 CEST 2015";
 
     public static final int TANGO_PAGES = 0;
     public static final int POGO_PAGES = 1;
