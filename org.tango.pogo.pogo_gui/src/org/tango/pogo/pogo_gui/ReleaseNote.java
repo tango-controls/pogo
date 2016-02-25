3392b32031b13c71b16c257e5b83f3aa677856be
Pascal Verdier <verdier@esrf.fr>
2016-02-25 15:15:04 +0000

    Use of entry_points in setup.py for better compatibility

--- org.tango.pogo.pogo_gui/src/org/tango/pogo/pogo_gui/ReleaseNote.java
+++ org.tango.pogo.pogo_gui/src/org/tango/pogo/pogo_gui/ReleaseNote.java
@@ -17,7 +17,11 @@ public interface ReleaseNote
 		"<BODY TEXT=\"#000000\" BGCOLOR=\"#FFFFFF\" LINK=\"#0000FF\" VLINK=\"#7F00FF\" ALINK=\"#FF0000\">\n" + 
 		"<P><!-------TITLE------></P>\n" + 
 		"<Center>	<h2>Pogo Release Note</h2>\n" + 
-		"	(Generated Thu Feb 04 10:47:34 CET 2016)</Center><Br>\n" + 
+		"	(Generated Thu Feb 25 16:10:03 CET 2016)</Center><Br>\n" + 
+		"<li><b>Pogo-9.2.3 - 25/02/2016:</b><Br>\n" + 
+		"&nbsp; &nbsp; Use of entry_points in setup.py for better compatibility.<Br>\n" + 
+		"<li><b>Pogo-9.2.2 - 04/02/2016:</b><Br>\n" + 
+		"&nbsp; &nbsp; Fix a bug in HTML generation with a command description empty<Br>\n" + 
 		"<li><b>Pogo-9.2.1 - 04/02/2016:</b><Br>\n" + 
 		"&nbsp; &nbsp; Improve Python and PythonHL templates for inheritance<Br>\n" + 
 		"<li><b>Pogo-9.2.0 - 20/01/2016:</b><Br>\n" + 
